26f380b686308d0444c1b4edfcd853f547161cdc
wangym0124 <wangym0124@gmail.com>
2017-02-13 21:36:31 -0800

    finish indexer

--- project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
+++ project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
@@ -23,26 +23,38 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 public class Indexer {
+    private static final String indexDirectory = "/var/lib/lucene/index1";
+    private IndexWriter indexWriter = null;
     
     /** Creates a new instance of Indexer */
     public Indexer() {
     }
  
+    private IndexWriter getIndexWriter() throws IOException {
+        if (indexWriter == null) {
+            Directory indexDir = FSDirectory.open(new File("indexDirectory"));
+            IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_2, new StandardAnalyzer());
+            indexWriter = new IndexWriter(indexDir, config);
+        }
+        return indexWriter;
+    }
+
+
     public void rebuildIndexes() {
 
         Connection conn = null;
 
         // create a connection to the database to retrieve Items from MySQL
-	try {
-	    conn = DbManager.getConnection(true);
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+    	try {
+    	    conn = DbManager.getConnection(true);
+    	} catch (SQLException ex) {
+    	    System.out.println(ex);
+    	}
 
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
+    	/*
+    	 * Add your code here to retrieve Items using the connection
+    	 * and add corresponding entries to your Lucene inverted indexes.
          *
          * You will have to use JDBC API to retrieve MySQL data from Java.
          * Read our tutorial on JDBC if you do not know how to use JDBC.
@@ -56,16 +68,47 @@ public class Indexer {
          * If you create new classes, make sure that
          * the classes become part of "edu.ucla.cs.cs144" package
          * and place your class source files at src/edu/ucla/cs/cs144/.
-	 * 
-	 */
+    	 * 
+    	 */
+        try {
+            conn = DriverManager.getConnection(true);
+            getIndexWriter();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT ItemID, Name, Description FROM Item");
+            PreparedStatement ps = conn.PreparedStatement("SELECT Category FROM ItemCategory WHERE ItemID = ?");
+            while (rs.next()) {
+                String itemID = rs.getString("ItemID");
+                String name = rs.getString("Name");
+                String description = rs.getString("Description");
+                String category = null;
+                ps.setString(1, itemID);
+                ResultSet result = ps.executeQuery();
+                while (result.next()) {
+                    category += result.getString("Category") + " ";
+                }
+                Document doc = new Document();
+
+                doc.add(new StringField("ItemID", itemID, Field.Store.YES));
+                doc.add(new StringField("Name", name, Field.Store.YES));
+                String searchContent = name + " " + description + " " + category;
+                doc.add(new TextField("searchContent", searchContent, Field.Store.NO));
+            }
+            indexWriter.addDocument(doc);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
 
         // close the database connection
-	try {
-	    conn.close();
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+    	try {
+            if (indexWriter != null) {
+                indexWriter.close();
+            }
+    	    conn.close();
+    	} catch (SQLException ex) {
+    	    System.out.println(ex);
+    	}
     }    
 
     public static void main(String args[]) {
