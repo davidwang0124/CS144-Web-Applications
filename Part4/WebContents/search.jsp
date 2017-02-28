d41e8d95609ce9ebe64d7571173d9bdb5acd4775
Xingan Wang <wangxgwxg@gmail.com>
2017-02-28 09:33:49 -0800

    search page

--- Part4/WebContents/search.jsp
+++ Part4/WebContents/search.jsp
@@ -1,9 +1,25 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
+    <meta charset="UTF-8">
     <title>Ebay Search</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    Search works!
+    <form class="search-form">
+        <input type="text" class="search"
+               value="${searchText}"
+               placeholder="Search by name, category, or description ">
+        <ul class="items">
+            <c:forEach items="${searchResults}" var="sr">
+                <li>
+                    <a href="">
+                        <span>${sr.getItemId()}</span>
+                        <span>${sr.getName()}</span>
+                    </a>
+                </li>
+            </c:forEach>
+        </ul>
+    </form>
 </body>
 </html>
--- Part4/src/edu/ucla/cs/cs144/SearchServlet.java
+++ Part4/src/edu/ucla/cs/cs144/SearchServlet.java
@@ -11,10 +11,22 @@ public class SearchServlet extends HttpServlet implements Servlet {
        
     public SearchServlet() {}
 
+    private Integer k = 20;
+    private Integer nextCount = 0;
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         // your codes here
-        // TODO: parse request for 'next' parameter
+        String searchText = request.getParameter("q");
+        String next = request.getParameter("next");
+        if (next != null && next.equals("1")) {
+            nextCount++;
+        } else {
+            nextCount = 0;
+        }
+        SearchResult[] sr = AuctionSearch.basicSearch(searchText, nextCount * k, (nextCount + 1) * k);
+        request.setAttribute("searchText", searchText);
+        request.setAttribute("searchResults", sr);
         request.getRequestDispatcher("/search.jsp").forward(request, response);
     }
 }
