50d1e157e4d76587f06608b17117f80d1d4e8525
Xingan Wang <wangxgwxg@gmail.com>
2017-03-02 13:29:02 -0800

    css restore a background none

--- Part4/WebContents/type-ahead.js
+++ Part4/WebContents/type-ahead.js
@@ -2,3 +2,7 @@ const endpointBase = "suggest?q=";
 function update(query) {
 
 }
+
+const inputBox = document.querySelector("input");
+inputBox.addEventListener("keyup", update);
+inputBox.addEventListener("change", update);
